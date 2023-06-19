054ee0537e168bf11bb83de545131b398377be2e
amancapy <amankp1701@gmail.com>
2023-06-20 01:52:06 +0530

    second first push

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rayon;
+use dashmap::*;
+
+
+struct Being{
+    pos: [f32; 2],
+    health: f32,
+
+    genome: [f32; 64],
+
+}
+
+struct World{
+
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
