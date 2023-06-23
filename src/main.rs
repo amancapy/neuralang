c899ea0a1848837f5e77006a7c16c8c7fd26c4fc
amancapy <amankp1701@gmail.com>
2023-06-24 01:55:43 +0530

    start of a visualizer

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use rayon::prelude::*;
 use dashmap::{DashMap, DashSet};
 use std::{sync::{Arc, Mutex}, ops::Deref};
 use rand::prelude::*;
+use minifb::{Key, Window, WindowOptions};
 
 
-const WIDTH: u32 = 720;
-const HEIGHT: u32 = 720;
-
+const WIDTH: usize = 720;
+const HEIGHT: usize = 720;
 
 
 #[derive(Debug)]
@@ -182,8 +182,28 @@ impl World {
     }
 }
 
-
 fn main() {
-    
-}
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        
+        
+
+
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
+}
