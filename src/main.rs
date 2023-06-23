6c43457da59dee4a80afb0252febb6ac676b4fa0
amancapy <amankp1701@gmail.com>
2023-06-23 23:59:08 +0530

    comt

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,6 @@ use dashmap::{DashMap, DashSet};
 use std::{sync::{Arc, Mutex}, ops::Deref};
 use rand::prelude::*;
 
-use error_iter::ErrorIter as _;
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture, wgpu::util::initialize_adapter_from_env_or_default};
-use winit::{dpi::LogicalSize, window::WindowBuilder};
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit_input_helper::WinitInputHelper;
-
 
 const WIDTH: u32 = 720;
 const HEIGHT: u32 = 720;
@@ -192,28 +184,6 @@ impl World {
 
 
 fn main() {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-
-        WindowBuilder::new().
-        with_title("world").
-        with_inner_size(size).
-        with_min_inner_size(size).
-        build(&event_loop)
-        .unwrap()
-    };
-
-
-    let mut pixels = {
-        let mut window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)
-    };
-
     
 }
 
