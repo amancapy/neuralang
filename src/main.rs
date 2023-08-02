f84cb81cd408d11084694fc4407e0645fa985df8
amancapy <amankp1701@gmail.com>
2023-08-02 23:27:56 +0530

    groundwork for NNs

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,50 @@ use slotmap::DefaultKey;
 use slotmap::SlotMap;
 use std::env;
 use std::f32::consts::PI;
-use std::ops::Index;
 use std::path;
 
-const W_SIZE: usize = 1000;
-const N_CELLS: usize = 250;
-const CELL_SIZE: usize = W_SIZE / N_CELLS;
-const W_FLOAT: f32 = W_SIZE as f32;
-const HZ: usize = 60;
+
+#[rustfmt::skip]
+mod consts {
+    pub const W_SIZE: usize = 1000;
+    pub const N_CELLS: usize = 250;
+    pub const CELL_SIZE: usize = W_SIZE / N_CELLS;
+    pub const W_FLOAT: f32 = W_SIZE as f32;
+    pub const HZ: usize = 60;
+
+    pub const B_RADIUS:                                 f32 = 3.;
+    pub const O_RADIUS:                                 f32 = 2.;
+    pub const F_RADIUS:                                 f32 = 2.;
+
+    pub const BASE_MOV_SPEED:                           f32 = 1.;
+    pub const BASE_ANG_SPEED_DEGREES:                   f32 = 10.;
+
+    pub const B_START_ENERGY:                           f32 = 10.;
+    pub const O_START_HEALTH:                           f32 = 5.;
+    pub const F_START_VAL:                              f32 = 2.;
+
+    pub const B_ENERGY_LEAK:                            f32 = 0.005;
+    pub const O_HEALTH_LEAK:                            f32 = 0.002;
+    pub const F_VAL_LEAK:                               f32 = 0.002;
+
+    pub const B_HEADON_DAMAGE:                          f32 = 0.25;
+    pub const B_REAR_DAMAGE:                            f32 = 1.;
+    pub const TAN_B_HITS_O_DAMAGE:                      f32 = 1.;
+    pub const HEADON_B_HITS_O_DAMAGE:                   f32 = 0.1;
+    pub const SPAWN_O_COST:                             f32 = 1.;                           // cost for a being to spawn an obstruct at their rear
+    pub const LOW_ENERGY_SPEED_DAMP_RATE:               f32 = 0.5;                          // beings slow down when their energy runs low
+    pub const OFF_DIR_MOVEMENT_SPEED_DAMP_RATE:         f32 = 0.5;                          // beings slow down when not moving face-forward
+
+    pub const N_FOOD_SPAWN_PER_STEP:                    usize = 1; 
+
+    pub const LANG_VEC_LEN:                           usize = 32;                           // length of the sound vector a being can emit
+    pub const B_OUTPUT_LEN:                           usize = LANG_VEC_LEN + 5;             // move_forward, rotate_left, rotate_right, spawn_obstruct, speak
+}
+
+use consts::*;
 
 
+// maps 2D space-partition index to 1D Vec index
 fn two_to_one((i, j): (usize, usize)) -> usize {
     i * N_CELLS + j
 }
@@ -26,10 +60,11 @@ fn dir_from_theta(theta: f32) -> Vec2 {
     Vec2::new(theta.cos(), theta.sin())
 }
 
-fn same_index((a, b): (usize, usize), (c, d): (usize, usize)) -> bool {
+fn same_partition_index((a, b): (usize, usize), (c, d): (usize, usize)) -> bool {
     a == c && b == d
 }
 
+// maps an entity's position to the cell that contains its centre
 pub fn pos_to_cell(pos: Vec2) -> (usize, usize) {
     let c = CELL_SIZE as f32;
     let i = ((pos[0] - (pos[0] % c)) / c) as usize;
@@ -54,6 +89,7 @@ pub fn bot_border_trespass(j: f32, r: f32) -> bool {
     j + r >= W_FLOAT - 1.
 }
 
+// out of bounds
 pub fn oob(ij: Vec2, r: f32) -> bool {
     let (i, j) = (ij[0], ij[1]);
     lef_border_trespass(i, r)
@@ -62,7 +98,7 @@ pub fn oob(ij: Vec2, r: f32) -> bool {
         || bot_border_trespass(j, r)
 }
 
-pub fn beings_collide(b1: &Being, b2: &Being) -> (f32, f32, Vec2) {
+pub fn b_collides_b(b1: &Being, b2: &Being) -> (f32, f32, Vec2) {
     let c1c2 = b2.pos - b1.pos;
     let centre_dist = c1c2.length();
     let (r1, r2) = (b1.radius, b2.radius);
@@ -70,14 +106,14 @@ pub fn beings_collide(b1: &Being, b2: &Being) -> (f32, f32, Vec2) {
     (r1 + r2 - centre_dist, centre_dist, c1c2)
 }
 
-pub fn obstruct_collide(b: &Being, o: &Obstruct) -> (f32, f32, Vec2) {
-    let c1c2 = b.pos - o.pos;
+pub fn b_collides_o(b: &Being, o: &Obstruct) -> (f32, f32, Vec2) {
+    let c1c2 = o.pos - b.pos;
     let centre_dist = c1c2.length();
     let (r1, r2) = (b.radius, o.radius);
     (r1 + r2 - centre_dist, centre_dist, c1c2)
 }
 
-pub fn food_collide(b: &Being, f: &Food) -> bool {
+pub fn b_collides_f(b: &Being, f: &Food) -> bool {
     let centre_dist = b.pos.distance(f.pos);
     let (r1, r2) = (b.radius, 1.);
     r1 + r2 - centre_dist > 0.
@@ -128,17 +164,15 @@ pub struct World {
     ob_id: usize,
     food_id: usize,
 
-    being_collision_count: usize,
-    obstruct_collision_count: usize,
-    food_collision_count: usize,
-
     being_deaths: Vec<(DefaultKey, Vec2)>,
     obstruct_deaths: Vec<(DefaultKey, Vec2)>,
     food_deaths: Vec<(DefaultKey, Vec2)>,
 
     age: usize,
 
-
+    being_collision_count: usize,
+    obstruct_collision_count: usize,
+    food_collision_count: usize,
 }
 
 impl World {
@@ -237,21 +271,17 @@ impl World {
         let rdist = Uniform::new(1., (W_SIZE as f32) - 1.);
         let mut rng = thread_rng();
 
-
-        // REMEMBER TO POS_UPDATE HERE INSTEAD OF ASYNC UPDATE (CURRENT)
         for _ in 0..substeps {
             let w = W_SIZE as f32;
             self.beings.iter_mut().for_each(|(k, being)| {
-                let move_vec = dir_from_theta(being.rotation) * (being.speed / s); 
+                let move_vec = dir_from_theta(being.rotation) * (being.speed / s); // this part to be redone based on being outputs
                 let newij = being.pos + move_vec;
 
-                let r = being.radius;
-
-                if !oob(newij, r) {
+                if !oob(newij, being.radius) {
                     being.pos_update = move_vec;
                 }
 
-                else { // TEMP TEMP TEMP TEMP NOTICE TEMP
+                else { // TEMP TEMP TEMP TEMP NOTICE TEMP TO BE FIXED
                     let newij = Vec2::new(rng.sample(rdist), rng.sample(rdist));
                     being.pos = newij;
                 }
@@ -263,10 +293,10 @@ impl World {
         let w = N_CELLS as isize;
 
         for i in 0..N_CELLS {
-            for j in 0..N_CELLS {
+            for j in 0..N_CELLS { // for each partition
                 let ij = two_to_one((i, j));
-                for id1 in &self.being_cells[ij] {
 
+                for id1 in &self.being_cells[ij] { // for each being
                     for (di, dj) in [
                         (-1, -1),
                         (-1, 0),
@@ -279,16 +309,17 @@ impl World {
                         (1, 1),
                     ] {
                         let (ni, nj) = ((i as isize) + di, (j as isize) + dj);
-                        if !(ni < 0 || ni >= w || nj < 0 || nj >= w) {
+
+                        if !(ni < 0 || ni >= w || nj < 0 || nj >= w) { // if valid partition
                             let (ni, nj) = (ni as usize, nj as usize);
                             let nij = two_to_one((ni, nj));
 
-                            for id2 in &self.being_cells[nij] {
+                            for id2 in &self.being_cells[nij] { // for another being in the same or one of the 8 neighbouring cells
                                 if !(id1 == id2) {
-                                    let (overlap, centre_dist, c1c2) = beings_collide(&self.beings.get(*id1).unwrap(), self.beings.get(*id2).unwrap());
+                                    let (overlap, centre_dist, c1c2) = b_collides_b(&self.beings.get(*id1).unwrap(), self.beings.get(*id2).unwrap());
+                                    
                                     if overlap > 0. {
                                         let b1 = self.beings.get_mut(*id1).unwrap();
-                                        self.being_collision_count += 1;
 
                                         let d_p = overlap / centre_dist * c1c2;
                                         let half_dist = 0.5 * d_p;
@@ -298,18 +329,20 @@ impl World {
                                         if !oob(new_pos, b1.radius) {
                                             b1.pos_update -= half_dist;
                                         }
+
+                                        self.being_collision_count += 1;
                                     }
                                 }
                             }
 
-                            for ob_id in &self.obstruct_cells[nij] {
-                                let b = self.beings.get_mut(*id1); // see this line here happens 3 times for some reason because the bc won't allow non overlapping borrows from the same vec even if i use a splitmut method the second time but i was assured the compiler compiles this away so who knows. for later.
+                            for ob_id in &self.obstruct_cells[nij] { // for an obstruct similarly
+                                let b = self.beings.get_mut(*id1);
                                 let o = self.obstructs.get_mut(*ob_id);
 
                                 let b_ref = b.as_ref().unwrap();
                                 let o_ref = o.as_ref().unwrap();
 
-                                let (overlap, centre_dist, c1c2) = obstruct_collide(b_ref, o_ref);
+                                let (overlap, centre_dist, c1c2) = b_collides_o(b_ref, o_ref);
                                 if overlap > 0. {
 
                                     let d_p = overlap / centre_dist * c1c2;
@@ -320,15 +353,15 @@ impl World {
                                 }
                             }
 
-                            for f_id in &self.food_cells[nij] {
+                            for f_id in &self.food_cells[nij] { // for a food similarly
 
-                                let b = self.beings.get_mut(*id1); // CAN'T INDEX BY ID ANYMORE :(
-                                let f = self.foods.get_mut(*f_id); //  ^
+                                let b = self.beings.get_mut(*id1);
+                                let f = self.foods.get_mut(*f_id);
 
                                 let b_ref = b.as_ref().unwrap();
                                 let f_ref = f.as_ref().unwrap();
                                 
-                                let overlap = food_collide(b_ref, f_ref);
+                                let overlap = b_collides_f(b_ref, f_ref);
                                 
 
                                 if overlap && !f_ref.eaten {
@@ -346,6 +379,7 @@ impl World {
         }
     }
 
+    // to reflect changes based on rotation, translation, collision resolution, fatigue, aging
     pub fn update_cells(&mut self) {
         for (k, b) in &mut self.beings {
             let new_pos = b.pos + b.pos_update;
@@ -362,7 +396,7 @@ impl World {
                 let (oi, oj) = b.cell;
                 let (i, j) = pos_to_cell(new_pos);
 
-                if !same_index((oi, oj), (i, j)) {
+                if !same_partition_index((oi, oj), (i, j)) {
                     b.cell = (i, j);
 
                     let oij = two_to_one((oi, oj));
@@ -375,6 +409,7 @@ impl World {
         }
     }
 
+    // being energy runs down
     pub fn tire_beings (&mut self, tire_rate: f32) {
         for (k, b) in &mut self.beings {
             b.energy -= tire_rate;
@@ -392,6 +427,7 @@ impl World {
         self.being_deaths.clear();
     }
 
+    // food grows stale and/or disappears
     pub fn age_foods(&mut self, age_rate: f32) {
         for (k, f) in &mut self.foods {
             f.age -= age_rate;
@@ -411,6 +447,7 @@ impl World {
         self.food_deaths.clear();
     }
 
+    // walls crack and/or crumble
     pub fn age_obstructs (&mut self, age_rate: f32) {
         for (k, o) in &mut self.obstructs {
             o.age -= age_rate;
@@ -428,7 +465,7 @@ impl World {
         self.obstruct_deaths.clear();
     }
 
-
+    // self-explanatory
     pub fn step(&mut self, substeps: usize) {
         for _ in 0..substeps {
             self.move_beings(substeps);
@@ -447,6 +484,7 @@ impl World {
 
 
 
+// a BUNCH of rendering boilerplate, will switch to Bevy rendering soon. wip.
 struct MainState {
     being_instances: graphics::InstanceArray,
     world: World,
@@ -466,7 +504,7 @@ impl MainState {
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut Context) -> Result<(), ggez::GameError> {
-        self.world.step(1);
+        self.world.step(8);
         if self.world.age % HZ == 0 {
             println!("timestep: {}, fps: {}", self.world.age, ctx.time.fps());
         }
@@ -481,7 +519,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
             let xy = b.pos;
             graphics::DrawParam::new()
                 .dest(xy.clone())
-                .scale(Vec2::new(1., 1.) / 400. * 2.)
+                .scale(Vec2::new(1., 1.) / 400. * B_RADIUS)
                 .rotation(b.rotation)
         }));
 
@@ -492,18 +530,19 @@ impl event::EventHandler<ggez::GameError> for MainState {
 }
 
 
+// a world populated as intended, this fn mainly to relieve World::new() of some clutter
 pub fn get_world () -> World {
     let mut world = World::new(N_CELLS);
     let rdist = Uniform::new(1., (W_SIZE as f32) - 1.);
     let mut rng = thread_rng();
 
-    for i in 1..50000 {
+    for i in 1..5000 {
         world.add_being(
-            2.,
+            B_RADIUS,
             Vec2::new(rng.sample(rdist), rng.sample(rdist)),
             rng.gen_range(-PI..PI),
 
-            0.2,
+            2.,
             1.
         );
     }
@@ -522,6 +561,8 @@ pub fn get_world () -> World {
 
 pub fn run() -> GameResult {
     assert!(W_SIZE % N_CELLS == 0);
+    assert!(B_RADIUS < CELL_SIZE as f32);
+
 
     let world = get_world();
 
@@ -556,6 +597,7 @@ pub fn run() -> GameResult {
     event::run(ctx, event_loop, state)
 }
 
+// to let it rip without rendering, mainly to gauge overhead of rendering over step() itself
 pub fn gauge(n: usize) {
     let mut w = get_world();
     for i in 0..n {
